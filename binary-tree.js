ca3b5f3f5eccbd7061e68bbcceb203e9b73b321e
VVIvan3 <vanya.vorobiev1@mail.ru>
2024-03-03 17:44:44 +0300

    Insertion + carcass of deletion

--- binary-tree.js
+++ binary-tree.js
@@ -23,10 +23,35 @@ class Tree {
     node.right = Tree.buildTree(array.slice(mid));
     return node;
   }
+
+  insertItem(item, node = this.root) {
+    if (node === null) {
+      node = new Node(item);
+      return node;
+    }
+    if (item === node.data) return node;
+    if (item < node.data) {
+      node.left = this.insertItem(item, node.left);
+    } else {
+      node.right = this.insertItem(item, node.right);
+    }
+    return node;
+  }
+
+  deleteItem(item, node = this.root) {
+    if (node === null) return node
+    if (item < node.data) {
+      node.left = this.deleteItem(item, node.left)
+    } else {
+      node.right = this.deleteItem(item, node.right)
+    }
+    // TODO
+  }
 }
+
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
-    return null;
+    return;
   }
   if (node.right !== null) {
     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
@@ -37,7 +62,10 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-
 const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const testTree = new Tree(testArr)
-prettyPrint(testTree.root)
+const testTree = new Tree(testArr);
+testTree.insertItem(2);
+testTree.insertItem(2);
+testTree.insertItem(10);
+testTree.insertItem(11);
+prettyPrint(testTree.root);
