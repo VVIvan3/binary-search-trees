6a07be82ed6f81f768f7c76c3dda9d6c4a56e8e1
VVIvan3 <vanya.vorobiev1@mail.ru>
2024-03-04 18:45:00 +0300

    Added ordering and deletion methods

--- binary-tree.js
+++ binary-tree.js
@@ -15,12 +15,20 @@ class Tree {
     return [...new Set(array.sort((a, b) => a - b))];
   }
 
-  static buildTree(array) {
-    if (array.length <= 1) return null;
-    const mid = parseInt(array.length / 2);
+  minimum(node) {
+    while (node.left !== null) {
+      node = node.left;
+    }
+    let minVal = node.data;
+    return minVal;
+  }
+
+  static buildTree(array, start = 0, end = array.length - 1) {
+    if (start > end) return null;
+    const mid = parseInt((start + end) / 2);
     const node = new Node(array[mid]);
-    node.left = Tree.buildTree(array.slice(0, mid));
-    node.right = Tree.buildTree(array.slice(mid));
+    node.left = Tree.buildTree(array, start, mid - 1);
+    node.right = Tree.buildTree(array, mid + 1, end);
     return node;
   }
 
@@ -39,16 +47,67 @@ class Tree {
   }
 
   deleteItem(item, node = this.root) {
-    if (node === null) return node
+    if (node === null) return node;
     if (item < node.data) {
-      node.left = this.deleteItem(item, node.left)
+      node.left = this.deleteItem(item, node.left);
+    } else if (item > node.data) {
+      node.right = this.deleteItem(item, node.right);
     } else {
-      node.right = this.deleteItem(item, node.right)
+      if (node.left === null) return node.right;
+      else if (node.right === null) return node.left;
+      else {
+        node.data = this.minimum(node.right);
+        node.right = this.deleteItem(node.data, node.right);
+      }
     }
-    // TODO
+    return node;
+  }
+
+  find(item, node = this.root) {
+    while (node !== null) {
+      if (item < node.data) node = node.left;
+      else if (item > node.data) node = node.right;
+      else if (item === node.data) return true;
+    }
+    return false;
+  }
+
+  levelOrder(node = this.root) {
+    const queue = [];
+    const array = [];
+    queue.push(node);
+    while (queue.length !== 0) {
+      const currentNode = queue[0];
+      array.push(currentNode.data);
+      if (currentNode.left !== null) queue.push(currentNode.left);
+      if (currentNode.right !== null) queue.push(currentNode.right);
+      queue.shift();
+    }
+    return array;
+  }
+
+  inOrder(node = this.root, array = []) {
+    if (node.left !== null) this.inOrder(node.left, array);
+    array.push(node.data);
+    if (node.right !== null) this.inOrder(node.right, array);
+    return array;
+  }
+
+  preOrder(node = this.root, array = []) {
+    array.push(node.data);
+    if (node.left !== null) this.preOrder(node.left, array);
+    if (node.right !== null) this.preOrder(node.right, array);
+    return array;
+  }
+
+  postOrder(node = this.root, array = []) {
+    if (node.left !== null) this.postOrder(node.left, array);
+    if (node.right !== null) this.postOrder(node.right, array);
+    array.push(node.data);
+    return array;
   }
 }
-
+//
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
     return;
@@ -63,9 +122,29 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 };
 
 const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const testTree = new Tree(testArr);
-testTree.insertItem(2);
-testTree.insertItem(2);
-testTree.insertItem(10);
-testTree.insertItem(11);
-prettyPrint(testTree.root);
+
+// const testTree = new Tree(testArr);
+// testTree.insertItem(2);
+// testTree.insertItem(2);
+// testTree.insertItem(10);
+// testTree.insertItem(11);
+// prettyPrint(testTree.root);
+
+// const testTreeTwo = new Tree(testArr);
+// prettyPrint(testTreeTwo.root);
+// testTreeTwo.deleteItem(8);
+// prettyPrint(testTreeTwo.root);
+// testTreeTwo.deleteItem(23);
+// prettyPrint(testTreeTwo.root);
+// testTreeTwo.deleteItem(4);
+// prettyPrint(testTreeTwo.root);
+
+const testTreeThree = new Tree(testArr);
+prettyPrint(testTreeThree.root);
+// console.log(testTreeThree.find(9));
+// console.log(testTreeThree.find(1));
+// console.log(testTreeThree.find(10));
+console.log(testTreeThree.levelOrder());
+console.log(testTreeThree.inOrder());
+console.log(testTreeThree.preOrder());
+console.log(testTreeThree.postOrder());
